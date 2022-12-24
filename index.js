be1fe572947175aeedac495f2a34fc91eb9e6b1b
Hani Amer <cvba2222@gmail.com>
2022-12-24 23:18:06 +0300

    assignment completed

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (firstName) => {
+  let newTutorials = tutorials.map(words => {
+
+    return words.split(" ").map(word => word[0].toUpperCase() + word.substring(1)).join(" ");
+  });
+  return newTutorials;
 }
